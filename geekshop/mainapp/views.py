32c305b42ff512eb4322a527a03168227d859ebb
Aleksey Shevchuk <send@ashevchuk.ru>
2022-01-27 22:54:40 +0300

    HomeWork2 complete!

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,9 +1,17 @@
+from email.charset import Charset
+import json
 from django.shortcuts import render
 
 # Create your views here.
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    with open ('./products.json', 'r', encoding='utf-8') as file:
+        products = json.load(file)
+    return render(request, 'mainapp/index.html', context = {
+        'title':'Главная',
+        'products': products
+    })
+
     
 
 def products(request):
@@ -11,4 +19,33 @@ def products(request):
     
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context = {
+        'title':'Контакты',
+    })
+
+def about(request):
+    return render(request, 'mainapp/about.html', context = {
+        'title':'О нас',
+    })
+
+def cart(request):
+    return render(request, 'mainapp/cart.html', context = {
+        'title':'Корзина',
+    })
+
+def base(request):
+    menu = [
+            {'name':'Шаурма1'},
+            {'name':'Шаурма2'},
+            {'name':'Шаурма3'},
+            {'name':'Шаурма4'},
+            {'name':'Шаурма5'},
+            {'name':'Шаурма6'},
+            {'name':'Шаурма7'},
+            {'name':'Шаурма8'},
+            {'name':'Шаурма9'},
+        ]
+    return render(request, 'mainapp/base.html', context= {
+        'menu': menu,
+        'title':'!секретная страница!'
+    })
